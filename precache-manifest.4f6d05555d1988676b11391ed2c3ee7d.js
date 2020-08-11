9aaecaff3b252283ed5d37b47ab24578bee19435
Rohit89205 <rohit892051182@gmail.com>
2020-08-11 10:38:29 +0530

    Updates

---
+++ precache-manifest.4f6d05555d1988676b11391ed2c3ee7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd33b37c9385034190f6505ed3480f9b",
+    "url": "/React-Map-App/index.html"
+  },
+  {
+    "revision": "c3faabe8add82dda33dc",
+    "url": "/React-Map-App/static/css/main.ec2dfb38.chunk.css"
+  },
+  {
+    "revision": "24acf8cea40ccde2c26e",
+    "url": "/React-Map-App/static/js/2.9cbc4326.chunk.js"
+  },
+  {
+    "revision": "c3faabe8add82dda33dc",
+    "url": "/React-Map-App/static/js/main.bc2e4a02.chunk.js"
+  },
+  {
+    "revision": "5616ee2d066bc6a1bfad",
+    "url": "/React-Map-App/static/js/runtime-main.935b284b.js"
+  }
+]);
